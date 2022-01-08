825bf78208f69682c7dd9ffe591a043428d02e56
fikrycryy <96042499+fikrycryy@users.noreply.github.com>
2022-01-08 09:56:21 +0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -3,3 +3,9 @@ document.getElementById("startOrStopImg").onclick = function() {
     if (audio.paused) audio.play();
     else audio.pause();
 };
+
+document.getElementById("startOrStopImg2").onclick = function() {
+    var audio = document.getElementById("audio_play2");
+    if (audio.paused) audio.play();
+    else audio.pause();
+};
